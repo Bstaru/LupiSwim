399fb5e409cc55bf1453547b8c756efca45b7f44
Natalie Conde Mejía <natalie.conde@moresoft.com>
2017-06-20 15:56:15 -0500

    Cambios en pantalla de trajes de baño

---
+++ js/animations.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+
+  /*  $(".btn1").click(function(){
+        $("p").slideUp();
+    });
+    $(".btn2").click(function(){
+        $("p").slideDown();
+    });*/
+
+
+	$("#MM").click(function(){
+	    $("#SB_M").toggle("slow");
+	});
+});
