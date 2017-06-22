d423d6113e881fcdddf02c17bc75f2e2681e3d0c
Natalie Conde <black_star_big@hotmail.com>
2017-06-22 18:39:55 -0500

    trajes.html y jquery ui

--- js/animations.js
+++ js/animations.js
@@ -1,16 +1,30 @@
 $(document).ready(function(){
 
+	$( function() {
+	    // run the currently selected effect
+	    function runEffect() {
 
 
-  /*  $(".btn1").click(function(){
-        $("p").slideUp();
-    });
-    $(".btn2").click(function(){
-        $("p").slideDown();
-    });*/
-
-
-	$("#MM").click(function(){
-	    $("#SB_M").toggle("slow");
-	});
+	      // get effect type from
+	      var selectedEffect = 'fade'
+	 
+	      // Most effect types need no options passed by default
+	      var options = {};
+	      // some effects have required parameters
+	      if ( selectedEffect === "scale" ) {
+	        options = { percent: 50 };
+	      } else if ( selectedEffect === "size" ) {
+	        options = { to: { width: 100, height: 100 } };
+	      }
+	 
+	      // Run the effect
+	       $( "#zoom1" ).css('z-index','2');
+	      $( "#zoom1" ).toggle( selectedEffect, options, 500 );
+	    };
+	 
+	    // Set effect from select menu value
+	    $( "#cuadros" ).hover(  function() {
+	      runEffect();
+	    });
+	  } );
 });
